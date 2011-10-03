b304926f80a79f0317d0fcaeb786e23747fe534e
Adrian Perez <aperez@igalia.com>
2011-10-04 00:09:43 +0300

    Add missing code in setup.py to allow registration in PyPI
    Added:
    
    - Usage of setuptools when it is available. Works just fine with
    	"distribute".
    - Package metadata.

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
-    name    = "roar",
-    version = "0.1",
-    scripts = ["roar"],
+    name         = "roar",
+    version      = "0.1",
+    scripts      = ["roar"],
+
+    description  = "Static website generator based on Growl",
+    url          = "http://github.com/Connectical/roar",
+    author       = "Adrian Perez",
+    author_email = "aperez@connectical.com",
+    classifiers  = ["Environment :: Console",
+                    "Development Status :: 4 - Beta",
+                    "Intended Audience :: Developers",
+                    "Intended Audience :: End Users/Desktop",
+                    "License :: OSI Approved :: GNU General Public License (GPL)",
+                    "Natural Language :: English",
+                    "Operating System :: OS Independent",
+                    "Programming Language :: Python :: 2",
+                    "Topic :: Internet :: WWW/HTTP :: Site Management"],
 )
